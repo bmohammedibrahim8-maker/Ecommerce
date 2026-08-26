38e8f3bd13639dd2ef161c4791a81bc5d89cedfb
bmoahammedibrahim8-maker <bmohammedibrahim8@gmail.com>
2026-08-26 10:22:34 +0530

    if.py

---
+++ if.py
@@ -0,0 +1,6 @@
+attendance=int(input("Enter attendance percentange:"))
+mark=int(input("Enter mark percentange:"))
+if (attendance >= 90 and mark >= 75):
+    print("Your Eligible")
+else:
+    print("Your Not Eligible")
